b014c529fe38a4138848ae938c09e2d0d5301307
marco-oliveira7 <ma8155354@gmail.com>
2025-01-31 23:40:29 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import List from "./components/List/list"
+import Search from "./components/Search/search"
+
+function App() {
+
+  return (
+    <div className="">
+      <Search></Search>
+      <List></List>
+    </div>
+  )
+}
+
+export default App
